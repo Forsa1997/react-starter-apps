e3703cf3e0c3417d2e6c8ec9b3401d39e0f8867c
Christoph Ruhee <Christoph.Ruhe@web.de>
2022-02-02 11:56:37 +0100

    Bootstrap

--- voting/src/App.js
+++ voting/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Voting from './Voting'
+import VoteBox from './components/VoteBox';
 
 
 function App() {
   return (
     <div className="App">
        <Voting />
+       <VoteBox />
     </div>
   );
 }
